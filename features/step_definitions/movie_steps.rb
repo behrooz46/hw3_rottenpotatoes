ae5af8bf953ebf8c2a116dd7c8c89b60fe35cb7a
Behrooz <behrooz.rabiee@gmail.com>
2012-08-11 19:04:47 +0000

    Feature Search added - #1

--- features/step_definitions/movie_steps.rb
+++ features/step_definitions/movie_steps.rb
@@ -5,6 +5,7 @@ Given /the following movies exist/ do |movies_table|
     # each returned element will be a hash whose key is the table header.
     # you should arrange to add that movie to the database here.
     m = Movie.create!(movie)
+    #puts movie
   end
   #flunk "Unimplemented"
 end
@@ -15,7 +16,22 @@ end
 Then /I should see "(.*)" before "(.*)"/ do |e1, e2|
   #  ensure that that e1 occurs before e2.
   #  page.content  is the entire content of the page as a string.
-  flunk "Unimplemented"
+  #flunk "Unimplemented"
+  assert page.body.index(e1) < page.body.index(e2) , "Wrong order"
+end
+
+Then /I should (not )?see movies rated: (.*)/ do |neg, rating_list|
+  ratings = rating_list.split(",")
+  if(neg)
+    ratings.each do |rating| 
+      page.find(:xpath, "//table[@id=\"movies\"]/tbody[count(tr[td = \"#{rating}\"]) = 0]")
+    end
+  else
+    Movie.all_ratings.each do |rating|
+      next if ratings.include?(rating)
+      page.find(:xpath, "//table[@id=\"movies\"]/tbody[count(tr[td = \"#{rating}\"]) = 0]")
+    end
+  end
 end
 
 # Make it easier to express checking or unchecking several boxes at once
@@ -26,4 +42,24 @@ When /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|
   # HINT: use String#split to split up the rating_list, then
   #   iterate over the ratings and reuse the "When I check..." or
   #   "When I uncheck..." steps in lines 89-95 of web_steps.rb
+  rating_list.split(",").each do |rating|
+    rating = "ratings_" + rating
+    if uncheck
+      uncheck(rating)
+    else
+      check(rating)
+    end
+  end
+end
+
+
+When /^I (un)?check all the ratings$/ do |uncheck|
+  Movie.all_ratings.each do |rating|
+    rating = "ratings_" + rating
+    if uncheck
+      uncheck(rating)
+    else
+      check(rating)
+    end
+  end
 end
